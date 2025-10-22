19efab3786d8e210ce4c5e58cb60126829ac5940
Emirhan LEVENTER <250542004@firat.edu.tr>
2025-10-22 23:04:49 +0300

    AsciiArtHarf.java

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,13 @@
+public class AsciiArtHarf {
+    public static void main(String[] args) {
+        // ASCII art ile ismimin bas harfini yazdırma
+        System.out.println("\nAscii Art Harf Cıktısı: \n");
+        System.out.println("#######");
+        System.out.println("#");
+        System.out.println("#");
+        System.out.println("#######");
+        System.out.println("#");
+        System.out.println("#");
+        System.out.println("#######");
+    }
+}
