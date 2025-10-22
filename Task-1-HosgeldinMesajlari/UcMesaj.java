8152e7d1405152a9214f187ab2168f2a2c437a49
Emirhan LEVENTER <250542004@firat.edu.tr>
2025-10-22 18:55:09 +0300

    UcMesaj.java

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,8 @@
+public class UcMesaj {
+        public static void main(String[] args) {
+            System.out.println("Hello World");
+            System.out.println("Java'ya Hoş Geldiniz");
+            System.out.println("Bilgisayar Bilimlerine Hoş Geldiniz");
+            System.out.println("Programlama Eğlencelidir");
+    }
+}
