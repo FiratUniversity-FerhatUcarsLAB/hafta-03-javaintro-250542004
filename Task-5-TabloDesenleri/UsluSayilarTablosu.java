7dbe9535823d741377233e1fcb5d43612e7567c4
Emirhan LEVENTER <250542004@firat.edu.tr>
2025-10-22 23:36:28 +0300

    UsluSayilarTablosu.java

---
+++ Task-5-TabloDesenleri/UsluSayilarTablosu.java
@@ -0,0 +1,16 @@
+public class UsluSayilarTablosu {
+    public static void main(String[] args) {
+        System.out.println("|========================|");
+        System.out.println("|    Uslu Sayi Tablosu   |");
+        System.out.println("|========================|");
+        System.out.println("|   x   |  x^2  |   x^3  |");
+        System.out.println("|========================|");
+        System.out.println("|   1   |   1    |  1    |");
+        System.out.println("|   2   |   4    |  8    |");
+        System.out.println("|   3   |   9    |  27   |");
+        System.out.println("|   4   |   16   |  64   |");
+        System.out.println("|   5   |   25   |  125  |");
+        System.out.println("|========================|");
+
+    }
+}
