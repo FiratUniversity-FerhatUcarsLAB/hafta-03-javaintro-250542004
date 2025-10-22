edd7ecc2bf181d85282a7ce6f1fd3ed694f151e7
Emirhan LEVENTER <250542004@firat.edu.tr>
2025-10-22 23:05:04 +0300

    IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,20 @@
+public class IsimKarti {
+    public static void main(String[] args) {
+        // Degerlerin atanmasi
+        String ad = "Emirhan";
+        String soyad = "LEVENTER";
+        String ogrenciNo = "250542004";
+        String bolum = "Yazilim Muh. (M.T.O.K.)";
+
+        // Degerlerin gösterilmesi
+
+        System.out.println("========================================");
+        System.out.println("              Ogrenci Karti             ");
+        System.out.println("========================================");
+        System.out.println(" Ad         : "+ ad);
+        System.out.println(" Soyad      : "+ soyad);
+        System.out.println(" Ogrenci No : "+ ogrenciNo);
+        System.out.println(" Bolum      : "+ bolum);
+        System.out.println("========================================");
+    }
+}
